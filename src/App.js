d6ef0a069553a2da67136652d09eaa944b43b923
347867019 <347867019@qq.com>
2020-06-15 23:03:11 +0800

    yes

--- src/App.js
+++ src/App.js
@@ -10,13 +10,24 @@ function App() {
     </div>
   );
 }
+var win = [
+  [0, 1, 2],
+  [3, 4 ,5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
 class Box extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
       lists: [-1,-1,-1,-1,-1,-1,-1,-1,-1],
       history:[],
-      item: false
+      item: false,
+      win:false
     }
     this.clicklattice = this.clicklattice.bind(this)
   }
@@ -26,7 +37,7 @@ class Box extends React.Component {
     // console.log(this.state.lists[index] === -1)
     // console.log(this.state.history)
     if(this.state.lists[index] !== -1)return true
-    const lists = this.state.lists
+    const lists = this.state.lists 
     if(this.state.item){
       lists[index] = 0
     }else{
@@ -38,13 +49,30 @@ class Box extends React.Component {
       lists: lists,
       item: !this.state.item,
       history: [...history]
+    }, function(){
+      const lists = this.state.lists 
+      // console.log(this.state.lists)
+      // console.log(win.length)
+      for(let i  = 0; i < win.length; i++){
+      //  console.log(win[i][0])
+      //  console.log(win[i][1])
+      //   console.log(win[i][2])
+        // if(win[i][0] ==)
+        if(lists[index] === lists[win[i][0]] && lists[index] === lists[win[i][1]] && lists[index] === lists[win[i][2]]){
+          this.setState({
+            win: lists[index]
+          })
+          return
+        }
+      }
+      
     })
   }
   fallback(index){
-    console.log(this.state.history)
+    // console.log(this.state.history)
     const lists = this.state.history[index]
     this.setState({
-      lists: lists,
+      lists: [...lists],
       item: index%2 === 0,
       history: this.state.history.slice(0,index+1)
     })
@@ -60,6 +88,8 @@ class Box extends React.Component {
           {this.state.history.map((item, index) => 
             <div key={index} onClick={this.fallback.bind(this, index)}>{index%2 === 0?'O执行完成，请X开始下棋':'X执行完成，请O开始下棋'}</div>
         )}
+        <div>{this.state.win === 1 ? 'O win' : ''}</div>
+        <div>{this.state.win === 0 ? 'X win' : ''}</div>
         </div>
       </div>
     )
